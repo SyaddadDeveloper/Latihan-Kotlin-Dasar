2c3745670c19e863682c86389c1e209cf2afdaf5
SyaddadDeveloper <134609697+SyaddadDeveloper@users.noreply.github.com>
2023-08-16 00:20:25 +0700

    belajar Data class
    Membuat array yang berisi data mahasiswa dengan bantuan data class

--- Fundamental.kt
+++ Fundamental.kt
@@ -1,6 +1,18 @@
-
+data class Mahasiswa(val name:String, var umur:Int, val status:Boolean)
 
 fun main() {
+    val TIB = arrayOf(
+        Mahasiswa("Syaddad",20,true),
+        Mahasiswa("maba1",20,true),
+        Mahasiswa("maba2",22,true)
+        )
+
+    var counter:Int = 0
+    while ( counter < TIB.size){
+        println(TIB[counter])
+        counter++
+    }
+
     val nama: String = "Syaddad"
     var status: String = "Mahasiswa"
     var umur: Int = 20
@@ -26,4 +38,47 @@ fun mahasiswa(nama: String,status: String, umur: Int, IPK: Double) {
     }else if(status == "tidak lulus" || (IPK < 1.5)){
         println("nama $nama telah dikeluarkan dari kampus")
     }
+
+    //Numbers
+    //Long (64bit)
+    var longNumber1:Long = 1000
+    var longNumber2 = 1000L
+
+    //Double (64bit)
+    var doubleNumber:Double = 0.534425
+
+    //Int (32bit)
+    var intNumber = 100
+
+    //Float (32bit)
+    var floatNumber:Float = 0.12345f
+
+    //Short (16bit)
+    var shortNumber: Short = 10
+
+    //Byte (8bit)
+    var byteNumber:Byte = 0b110100
+
+    //Array
+    val array1 = arrayOf(1,"kotlin", true)
+    val array2 = intArrayOf(1,8,3,4,5)
+    array2[1] = 2
+
+    //null
+    val nullVariable: String? = null
+    if (nullVariable != null) {
+        println(nullVariable)
+    }
+
+
+println(Color.RED.value)
+
+}
+
+    //enumeration
+
+enum class Color(val value:Int){
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF)
 }
